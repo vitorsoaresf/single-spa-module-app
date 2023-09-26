1dfabd5a37cb076e0679807f1fd913cdaaaa0858
Vitor Soares <vitorsoaresf@gmail.com>
2023-09-26 17:16:52 -0300

    feat: change port webpack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const App = () => {
-  return <div>App</div>;
+export const App = ({ name }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h3>Contador = {count}</h3>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      <button onClick={() => setCount((count) => count - 1)}>-</button>
+    </div>
+  );
 };
